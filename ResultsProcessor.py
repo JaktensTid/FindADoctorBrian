c36cbd45579f70674e33e6be70e9c53c7ccdb4dd
JaktensTid <jaktenstid1@gmail.com>
2017-02-22 15:28:55 +0300

    Init

---
+++ ResultsProcessor.py
@@ -0,0 +1,76 @@
+from lxml import html
+from lxml import etree
+import requests
+import threading
+import csv
+
+xpath_a_hrefs = "//div[@class='cyno-btn-wrapper']/a/@href"
+
+def get_links_to_doctors_page(response):
+    document = html.fromstring(response.content)
+    return document.xpath(xpath_a_hrefs)
+
+class DoctorPageProcessor():
+    xpath_title = '//div[@property="dc:title"]'
+    xpath_div_a_href = '//div[@class="field field-name-field-listing-website field-type-link-field field-label-hidden"]/div/div/a/@href'
+    xpath_div_where_content = '//div[@class="content"]'
+    xpath_div_first_name = '//div[@class="field field-name-field-pract-fname field-type-text field-label-hidden"]/div/div/text()'
+    xpath_div_mi = '//div[@class="field field-name-field-pract-mi field-type-text field-label-hidden"]/div/div/text()'
+    xpath_div_last_name = '//div[@class="field field-name-field-pract-lname field-type-text field-label-hidden"]/div/div/text()'
+
+    def __init__(self, doctor_page_links, corresponding_zip):
+        self.doctor_page_links = doctor_page_links
+        self.corresponding_zip = corresponding_zip
+
+    def run(self):
+        clinics = self.get_corresponding_records()
+        with open(self.corresponding_zip +'.csv', 'w', newline='', encoding="utf-8") as csvfile:
+            titles = ["Clinic", "Link", "Doctor"]
+            writer = csv.DictWriter(csvfile, fieldnames=titles)
+            writer.writeheader()
+            for doctor,info in clinics.items():
+                writer.writerow({"Clinic": doctor, "Link": info[0], "Doctor": info[1]})
+
+    def get_corresponding_records(self):
+        result = {}
+        for d_link in self.doctor_page_links:
+            response = requests.get(d_link)
+            document = html.fromstring(response.content)
+            if response.status_code != 200:
+                print("*************** STATUS CODE INVALID " + d_link + " **********************")
+            try:
+                link = document.xpath(self.xpath_div_a_href)[0]
+            except IndexError:
+                link = " "
+            title = document.xpath(self.xpath_title)[0].text_content()
+            doctor = self.get_doctor(document, d_link)
+            debug_string = ("Page: {0:20}, Link: {1:20}, Title: {2:30}, Zip: {3:5}, Doctor: {4:20}").\
+                format(d_link, link, title, self.corresponding_zip,doctor)
+            print(debug_string)
+            result[title] = (link,doctor)
+        return result
+
+    def get_doctor(self, document, locator):
+        divs = document.xpath(self.xpath_div_where_content)
+        div_content = divs[1]
+        repr = etree.tostring(div_content)
+        first_name = ""
+        mi_name = ""
+        last_name = ""
+        try:
+            first_name = div_content.xpath(self.xpath_div_first_name)[0]
+        except IndexError:
+            print("**** INDEX ERROR "+ locator + " *******")
+        mi_names = div_content.xpath(self.xpath_div_mi)
+        if len(mi_names) != 0:
+            mi_name = mi_names[0]
+        try:
+            last_name = div_content.xpath(self.xpath_div_last_name)[0]
+        except IndexError:
+            print("**** INDEX ERROR " + locator + " *******")
+        full_name = first_name + " " + mi_name + " " + last_name if mi_name != "" else first_name + " " + last_name
+        if full_name == " ":
+            full_name = " "
+        return full_name
+
+
